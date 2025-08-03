f76f4b9f5ec922856990e2aa31a9976cdaa10e64
Mete <69116434+metebykl@users.noreply.github.com>
2025-08-03 16:17:57 +0300

    refactor: recursive dependency resolver

--- src/grintfile.rs
+++ src/grintfile.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashSet, VecDeque};
+use std::collections::HashSet;
 use std::fs;
 use std::path::{Path, PathBuf};
 
@@ -71,46 +71,55 @@ impl Grintfile {
     arguments: &[String],
   ) -> Result<(), Box<dyn std::error::Error>> {
     let mut ran = HashSet::new();
-    let mut queue: VecDeque<String> = arguments.iter().cloned().collect();
 
-    while let Some(name) = queue.pop_front() {
-      if ran.contains(&name) {
-        continue;
+    for name in arguments {
+      if !ran.contains(name) {
+        self.run_task_with_dependencies(config, name, &mut ran, &mut Vec::new())?;
       }
+    }
 
-      let mut stack = vec![name.clone()];
-      let mut visited = HashSet::new();
-      visited.insert(name.clone());
+    Ok(())
+  }
 
-      while let Some(current_name) = stack.last().cloned() {
-        let task = self
-          .tasks
-          .get(&current_name)
-          .ok_or_else(|| format!("task '{}' not found", current_name))?;
+  fn run_task_with_dependencies(
+    &self,
+    config: &Config,
+    name: &str,
+    ran: &mut HashSet<String>,
+    path: &mut Vec<String>,
+  ) -> Result<(), Box<dyn std::error::Error>> {
+    let task = self
+      .tasks
+      .get(name)
+      .ok_or_else(|| format!("task '{}' not found", name))?;
 
-        let mut all_dependencies_ran = true;
-        for dependency in &task.dependencies {
-          if ran.contains(dependency) {
-            continue;
-          }
+    if let Some(cycle_start) = path.iter().position(|n| n == name) {
+      let cycle: Vec<&str> = path[cycle_start..].iter().map(|s| s.as_str()).collect();
+      return Err(
+        format!(
+          "dependency cycle detected: {} -> {}",
+          cycle.join(" -> "),
+          name
+        )
+        .into(),
+      );
+    }
 
-          if visited.contains(dependency) {
-            return Err(format!("detected dependency cycle involving '{}'", dependency).into());
-          }
+    if ran.contains(name) {
+      return Ok(());
+    }
 
-          visited.insert(dependency.clone());
-          stack.push(dependency.clone());
-          all_dependencies_ran = false;
-        }
+    path.push(name.to_string());
 
-        if all_dependencies_ran {
-          stack.pop();
-          if !ran.contains(&current_name) {
-            task.run(config, self)?;
-            ran.insert(current_name);
-          }
-        }
-      }
+    for dependency in &task.dependencies {
+      self.run_task_with_dependencies(config, dependency, ran, path)?;
+    }
+
+    path.pop();
+
+    if !ran.contains(name) {
+      task.run(config, self)?;
+      ran.insert(name.to_string());
     }
 
     Ok(())
